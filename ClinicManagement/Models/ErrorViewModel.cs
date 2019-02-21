55e8c1c6064b41573a69635c11f3cc03442634a8
unknown <doanncd00530@fpt.edu.vn>
2019-02-21 20:20:01 +0700

    Khởi tạo project.

---
+++ ClinicManagement/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ClinicManagement.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
